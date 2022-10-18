dc13d2aa641494189462ca1d1451006d533c50b4
cydemexsit <jeremylu2010@gmail.com>
2022-10-18 23:58:40 +0800

    1018update

---
+++ app/src/main/java/com/example/democonstraintlayout/CActivity.java
@@ -0,0 +1,13 @@
+package com.example.democonstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cactivity);
+    }
+}
